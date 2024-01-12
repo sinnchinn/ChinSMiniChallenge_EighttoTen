bc66e78f4a580c25a3faf24716fefc85c20df397
sinnchinn <sinathachin@gmail.com>
2024-01-11 16:33:28 -0800

    fixing per peer review

--- Services/MiniChallenge9_3/MiniCh9_3Service.cs
+++ Services/MiniChallenge9_3/MiniCh9_3Service.cs
@@ -6,7 +6,7 @@ public class MiniCh9_3Service : IMiniCh9_3Service
     public string HardMode(string hardGuess)
     {
         bool isTrue = int.TryParse(hardGuess, out int realHardGuess);
-        int hardRandom = new Random().Next(0, 50);
+        int hardRandom = new Random().Next(0, 101);
 
 
         if (isTrue == true)
